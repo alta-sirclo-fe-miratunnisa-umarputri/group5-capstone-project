c66d5f4c920bdcd53692f55c85a590c350aae167
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-14 14:56:51 +0700

    refactor: remove sign style file

--- src/components/Sign/Greetings.style.ts
+++
@@ -1,9 +0,0 @@
-import { SxProps } from "@mui/material";
-
-import { primary } from "../../styles/color.styles";
-
-export const titleAndSubtitle: SxProps = {
-  ...primary,
-  fontFamily: "Poppins",
-  fontWeight: "medium",
-};
