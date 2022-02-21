0cd578e3366b4225a338e6ef941a386ca337670a
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-21 11:38:50 +0700

    feat: add dummy data

---
+++ src/dummy-data.ts
@@ -0,0 +1,122 @@
+export const dummyAssets = [
+  {
+    kategori: "laptop",
+    judul: "lenovo think pad",
+    gambar:
+      "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo idea pad",
+    gambar:
+      "https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo think pad",
+    gambar:
+      "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "elektronik",
+    judul: "kipas cosmos wadesta",
+    gambar:
+      "https://images.unsplash.com/photo-1565151443833-29bf2ba5dd8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "elektronik",
+    judul: "kipas maspion",
+    gambar:
+      "https://images.unsplash.com/photo-1568392816241-44d223d4d490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "alat tulis",
+    judul: "papan tulis",
+    gambar:
+      "https://images.unsplash.com/photo-1589739900869-082b93d8f224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=396&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "elektronik",
+    judul: "kipas maspion",
+    gambar:
+      "https://images.unsplash.com/photo-1568392816241-44d223d4d490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo idea pad",
+    gambar:
+      "https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo think pad",
+    gambar:
+      "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo idea pad",
+    gambar:
+      "https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "elektronik",
+    judul: "kipas maspion",
+    gambar:
+      "https://images.unsplash.com/photo-1568392816241-44d223d4d490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+  {
+    kategori: "laptop",
+    judul: "lenovo think pad",
+    gambar:
+      "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    deskripsi:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    pengguna: ["user 1", "user 2", "user 3", "user 4", "user 5", "user 6"],
+    tersedia: 102,
+  },
+];
