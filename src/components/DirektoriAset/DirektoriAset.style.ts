30ff6800fc47779664eb33d03024f1454ac4102a
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-23 10:56:57 +0700

    feat: create direktori aset style

---
+++ src/components/DirektoriAset/DirektoriAset.style.ts
@@ -0,0 +1,9 @@
+import { SxProps } from "@mui/material";
+
+export const containerDir: SxProps = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  mt: 3,
+};
--- src/pages/DirektoriAset.tsx
+++ src/pages/DirektoriAset.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 
 import ContentContainer from "../components/ContentContainer";
 import CardAsset from "../components/DirektoriAset/CardAsset";
+import { containerDir } from "../components/DirektoriAset/DirektoriAset.style";
 import SearchBar from "../components/DirektoriAset/SearchBar";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
@@ -21,15 +22,7 @@ const DirektoriAset = () => {
       <Outlet />
 
       <ContentContainer>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            mt: 3,
-          }}
-        >
+        <Box sx={containerDir}>
           <SearchBar />
           <CardAsset assets={dummyAssets} />
         </Box>
