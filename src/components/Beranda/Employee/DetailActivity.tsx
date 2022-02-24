da3b07089f01587ec7871ff4901ae853ac223bfd
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-24 14:54:35 +0700

    feat: create detail activity component

--- src/components/Beranda/Employee/DetailActivity.tsx
+++ src/components/Beranda/Employee/DetailActivity.tsx
@@ -8,28 +8,34 @@ import {
   Box,
   useMediaQuery,
   useTheme,
+  Grid,
+  Avatar,
+  DialogActions,
 } from "@mui/material";
 
-import {
-  CustomFormRead,
-  CustomFormReadMulti,
-} from "../../DirektoriAset/CustomFormRead";
 import { DetailAndEmployeeModal } from "../../../types/direktori-aset";
+import { title } from "../../DirektoriAset/DetailAndUser.style";
+import { primary } from "../../../styles/color.styles";
+import DetailActivityInfo from "./DetailActivityInfo";
 import {
-  buttonActions,
-  containerActionsDetail,
-  detailImg,
-  title,
-} from "../../DirektoriAset/DetailAndUser.style";
+  availabilityFont,
+  avatar,
+  avatarContainer,
+  backButton,
+  buttonContainer,
+  cancellationButton,
+  categoryFont,
+  itemFont,
+} from "./DetailActivity.style";
 
 const DetailActivity = ({ isOpen, handleClose }: DetailAndEmployeeModal) => {
-  const { id } = useParams();
+  // const { id } = useParams();
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-  // dummy
-  const isMaintenance = false;
-  console.log(id);
+  const handleCancellation = () => {
+    console.log("batalkan pengajuan");
+  };
 
   return (
     <Dialog
@@ -41,52 +47,77 @@ const DetailActivity = ({ isOpen, handleClose }: DetailAndEmployeeModal) => {
       <DialogTitle>
         <Box>
           <Typography variant="h5" sx={title}>
-            Detail Aset
+            Detail Permohonan
           </Typography>
         </Box>
       </DialogTitle>
+
       <DialogContent>
-        <img
-          src="https://source.unsplash.com/random"
-          alt="Aset"
-          style={detailImg}
-        />
-        <CustomFormRead label="Nama Aset" defaultValue="Lenovo" />
-        <CustomFormReadMulti
-          label="Deskripsi Aset"
-          defaultValue="Ini adalah laptop yang akan digunakan untuk coding"
-        />
-        <CustomFormRead label="Kategori Aset" defaultValue="Laptop" />
+        <Grid container>
+          <Grid item xs={5} sx={avatarContainer}>
+            <Avatar
+              src="https://source.unsplash.com/random"
+              alt="Item"
+              sx={avatar}
+            />
+          </Grid>
 
-        <Box sx={containerActionsDetail}>
-          {isMaintenance ? (
-            <Button variant="contained" size="small" sx={buttonActions}>
-              Pemeliharaan Aset Selesai
-            </Button>
-          ) : (
-            <Button variant="contained" size="small" sx={buttonActions}>
-              Pemeliharaan Aset
-            </Button>
-          )}
+          <Grid item xs={7} pl={3}>
+            <Typography variant="body2" gutterBottom sx={categoryFont}>
+              Kipas Angin
+            </Typography>
+            <Typography variant="h5" gutterBottom sx={itemFont}>
+              Kipas Angin Maspion Shgasdasdhj Ssvjavadsjak
+            </Typography>
+            <Typography variant="caption" sx={availabilityFont}>
+              16 tersedia
+            </Typography>
+          </Grid>
+        </Grid>
 
-          <Button
-            variant="contained"
-            size="small"
-            sx={buttonActions}
-            onClick={() => handleClose(true)}
-          >
-            Kembali
-          </Button>
-        </Box>
+        <Grid container mt={2}>
+          <Grid item xs={5}>
+            <DetailActivityInfo label="Pemohon" description="Ratu" />
+            <DetailActivityInfo
+              label="Status Pengajuan"
+              description="Disetujui"
+            />
+          </Grid>
+
+          <Grid item xs={7}>
+            <DetailActivityInfo
+              label="Waktu Pengajuan"
+              description={new Date().toLocaleString()}
+            />
+            <DetailActivityInfo
+              label="Keterangan"
+              description=" Curabitur venenatis enim quis tellus feugiat blandit. Vivamus
+              libero libero, fringilla vel elementum at, viverra nec ante."
+            />
+          </Grid>
+        </Grid>
+
+        <DialogActions>
+          <Grid container mt={2}>
+            <Grid item xs={4}></Grid>
+            <Grid item xs={8} sx={buttonContainer}>
+              <Button sx={cancellationButton} onClick={handleCancellation}>
+                Batalkan Pengajuan
+              </Button>
+              <Button
+                variant="contained"
+                sx={backButton}
+                onClick={() => handleClose(true)}
+              >
+                Kembali
+              </Button>
+            </Grid>
+            <Grid item></Grid>
+          </Grid>
+        </DialogActions>
       </DialogContent>
     </Dialog>
   );
 };
 
 export default DetailActivity;
-
-// const DetailActivity = () => {
-//   return <h3>Detail Activity</h3>;
-// };
-
-// export default DetailActivity;
