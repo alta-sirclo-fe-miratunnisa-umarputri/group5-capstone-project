489a450658e7f21313ef7e935623b34551f74275
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-24 14:59:58 +0700

    feat: create detail activity info component

---
+++ src/components/Beranda/Employee/DetailActivityInfo.tsx
@@ -0,0 +1,23 @@
+import { Typography } from "@mui/material";
+
+import { labelFont, descriptionFont } from "./DetailActivityInfo.style";
+
+type Info = {
+  label: string;
+  description: string;
+};
+
+const DetailActivityInfo = ({ label, description }: Info) => {
+  return (
+    <>
+      <Typography variant="subtitle1" sx={labelFont}>
+        {label}
+      </Typography>
+      <Typography gutterBottom variant="subtitle1" sx={descriptionFont}>
+        {description}
+      </Typography>
+    </>
+  );
+};
+
+export default DetailActivityInfo;
