b36a48f747e984107bf8c7fa022c64edce6bc790
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-24 21:08:13 +0700

    feat: create application modal component without asset category input and others input

---
+++ src/components/Beranda/Employee/Application.tsx
@@ -0,0 +1,78 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  Box,
+  useMediaQuery,
+  useTheme,
+  DialogActions,
+  Button,
+} from "@mui/material";
+
+import { generalFont } from "../../../styles/font.style";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CustomFormInput from "../../CustomFormInput";
+
+const Application = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const navigate = useNavigate();
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleClose = () => {
+    setIsOpen(false);
+    navigate("/beranda");
+  };
+
+  const handleApplication = () => {
+    console.log("ajukan peminjaman");
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      fullScreen={fullScreen}
+      fullWidth
+    >
+      <DialogTitle>
+        <Box>
+          <Typography variant="h5" sx={generalFont}>
+            Peminjaman Aset
+          </Typography>
+        </Box>
+      </DialogTitle>
+
+      <DialogContent>
+        <CustomFormInput
+          label="Spesifikasi Kebutuhan"
+          type="text"
+          desc="spesifikasi-kebutuhan"
+          placeholder="Deskripsikan barang yang dibutuhkan"
+        />
+        <CustomFormInput
+          label="Keterangan"
+          type="text"
+          desc="keterangan"
+          placeholder="Deskripsikan tujuan penggunaan barang yang akan dipinjam"
+        />
+        <DialogActions>
+          <Button onClick={handleClose}>Batal</Button>
+          <Button onClick={handleApplication}>Ajukan Peminjaman</Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Application;
+
+// const Application = () => {
+//   return <h1>Application</h1>;
+// };
+
+// export default Application;
