b83edb7ea5e43e39a59fda0aa8fbbc036793b19a
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-28 11:21:35 +0700

    feat: create admin directory asset pagination

--- src/components/DirektoriAset/CardAsset.tsx
+++ src/components/DirektoriAset/CardAsset.tsx
@@ -69,108 +69,86 @@ const CardAsset = ({ assets, role }: any) => {
   return (
     <>
       <Masonry columns={{ xs: 1, sm: 3, md: 5 }} spacing={2} sx={masonry}>
-        {assets.map((asset: any) => (
-          <Box key={asset.id}>
-            <Card sx={card}>
-              <CardMedia component="img" image={asset.gambar} alt="random" />
+        {assets &&
+          assets.map((asset: any) => (
+            <Box key={asset.id}>
+              <Card sx={card}>
+                <CardMedia component="img" image={asset.picture} alt="random" />
 
-              <CardContent sx={cardContent}>
-                <Typography variant="caption" sx={category}>
-                  {asset.kategori}
-                </Typography>
-                <Typography variant="h6" sx={title}>
-                  {asset.judul}
-                </Typography>
-                <Typography variant="body2" sx={description}>
+                <CardContent sx={cardContent}>
+                  <Typography variant="caption" sx={category}>
+                    {asset.category}
+                  </Typography>
+                  <Typography variant="h6" sx={title}>
+                    {asset.name}
+                  </Typography>
+                  {/* <Typography variant="body2" sx={description}>
                   {asset.deskripsi}
-                </Typography>
+                </Typography> */}
+                  <Typography sx={availability} textAlign="end">
+                    {asset.availableStatus}
+                  </Typography>
+                </CardContent>
 
-                <Grid container mt={2} mb={0} sx={aboveButton}>
-                  <Grid item xs={6} sx={groupAvatar}>
-                    <Avatar
-                      alt="A"
-                      src="https://source.unsplash.com/random"
-                      sx={avatar}
-                    />
-                    <Avatar
-                      alt="A"
-                      src="https://source.unsplash.com/random"
-                      sx={avatar}
-                    />
-                    <Avatar alt="A" sx={avatar}>
-                      <Typography variant="caption" sx={avatarText}>
-                        {asset.pengguna.length - 2}+
-                      </Typography>
-                    </Avatar>
-                  </Grid>
-
-                  <Grid item xs={6} textAlign="end">
-                    <Typography sx={availability}>
-                      {asset.tersedia} tersedia
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-
-              <CardActions sx={cardActions}>
-                {role === ROLE.ADMIN && (
-                  <Grid container spacing={1}>
-                    <Grid item xs={12} lg={6}>
-                      <Link
-                        to={`/direktori-aset/pengguna`}
-                        style={{ textDecoration: "none", width: "100%" }}
-                      >
-                        <Button
-                          variant="contained"
-                          size="small"
-                          fullWidth
-                          sx={button}
-                          onClick={handleOpenUser}
+                <CardActions sx={cardActions}>
+                  {role === ROLE.ADMIN && (
+                    <Grid container spacing={1}>
+                      <Grid item xs={12} lg={6}>
+                        <Link
+                          to={`/direktori-aset/pengguna`}
+                          style={{ textDecoration: "none", width: "100%" }}
                         >
-                          Pengguna
-                        </Button>
-                      </Link>
-                    </Grid>
+                          <Button
+                            variant="contained"
+                            size="small"
+                            fullWidth
+                            sx={button}
+                            onClick={handleOpenUser}
+                          >
+                            Pengguna
+                          </Button>
+                        </Link>
+                      </Grid>
 
-                    <Grid item xs={12} lg={6}>
-                      <Link
-                        to={`/direktori-aset/admin/${asset.id}`}
-                        style={{ textDecoration: "none", width: "100%" }}
-                      >
-                        <Button
-                          variant="contained"
-                          size="small"
-                          fullWidth
-                          sx={button}
-                          onClick={handleOpenDetailItem}
+                      <Grid item xs={12} lg={6}>
+                        <Link
+                          to={`/direktori-aset/admin/${asset.id}`}
+                          style={{ textDecoration: "none", width: "100%" }}
                         >
-                          Detail
-                        </Button>
-                      </Link>
+                          <Button
+                            variant="contained"
+                            size="small"
+                            fullWidth
+                            sx={button}
+                            onClick={handleOpenDetailItem}
+                          >
+                            Detail
+                          </Button>
+                        </Link>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                )}
+                  )}
 
-                {role === ROLE.EMPLOYEE && (
-                  <Link
-                    to={`/direktori-aset/employee/${asset.id}`}
-                    style={{ textDecoration: "none", width: "100%" }}
-                  >
-                    <Button
-                      variant="contained"
-                      size="small"
-                      fullWidth
-                      sx={button}
-                      onClick={handleOpenDetailAsset}
+                  {role === ROLE.EMPLOYEE && (
+                    <Link
+                      to={`/direktori-aset/employee/${asset.id}`}
+                      style={{ textDecoration: "none", width: "100%" }}
                     >
-                      Detail
-                    </Button>
-                  </Link>
-                )}
-              </CardActions>
-            </Card>
-          </Box>
-        ))}
+                      <Button
+                        variant="contained"
+                        size="small"
+                        fullWidth
+                        sx={button}
+                        onClick={handleOpenDetailAsset}
+                      >
+                        Detail
+                      </Button>
+                    </Link>
+                  )}
+                </CardActions>
+              </Card>
+            </Box>
+          ))}
       </Masonry>
 
       <DetailItemAdmin
--- src/pages/DirektoriAset.tsx
+++ src/pages/DirektoriAset.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Pagination } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 import ContentContainer from "../components/ContentContainer";
@@ -9,9 +9,51 @@ import Layout from "../components/Layout";
 
 import { dummyAssets } from "../dummy-data";
 import { containerDir } from "../components/DirektoriAset/DirektoriAset.style";
+import { useState } from "react";
+import { capstoneAxios } from "../axios-instance";
+import { useQuery } from "react-query";
+import Loading from "../components/Loading";
+import Error from "../components/Alert/Error";
+import { AxiosError } from "axios";
 
 const DirektoriAset = () => {
   const role = localStorage.getItem("role")!;
+
+  const [page, setPage] = useState(1);
+
+  const { isLoading, error, isError, data } = useQuery(
+    ["directoryAssetAdmin", page],
+    async () => {
+      const { data } = await capstoneAxios({
+        method: "GET",
+        url: "/items",
+        params: {
+          page,
+        },
+      });
+
+      return data;
+    }
+  );
+
+  if (isLoading) {
+    return (
+      <Box sx={{ marginTop: 25 }}>
+        <Loading />
+      </Box>
+    );
+  }
+
+  if (isError) {
+    return <Error message={(error as AxiosError).response!.data!.message!} />;
+  }
+
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
 
   return (
     <Layout>
@@ -22,12 +64,19 @@ const DirektoriAset = () => {
         consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
         adipiscing elit."
       />
+
       <Outlet />
 
       <ContentContainer>
         <Box sx={containerDir}>
           <SearchBar />
-          <CardAsset assets={dummyAssets} role={role} />
+          <Pagination
+            count={data.data.totalPage}
+            sx={{ marginTop: 3, marginBottom: 1 }}
+            onChange={handleChangePage}
+            page={page}
+          />
+          <CardAsset assets={data.data.items} role={role} />
         </Box>
       </ContentContainer>
     </Layout>
