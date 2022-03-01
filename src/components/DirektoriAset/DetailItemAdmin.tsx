9c133aaeedf85d58a19454f99c718a0ae17e44d6
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-03-01 23:33:23 +0700

    feat: update items status

--- src/components/DirektoriAset/DetailItemAdmin.tsx
+++ src/components/DirektoriAset/DetailItemAdmin.tsx
@@ -1,4 +1,6 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { AxiosError } from "axios";
 import {
   Button,
   Dialog,
@@ -9,8 +11,13 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 import { CustomFormRead, CustomFormReadMulti } from "./CustomFormRead";
+import Error from "../Alert/Error";
+import Loading from "../Loading";
+import Success from "../Alert/Success";
+
 import { DetailAndEmployeeModal } from "../../types/direktori-aset";
 import {
   buttonActions,
@@ -18,24 +25,91 @@ import {
   detailImg,
   title,
 } from "./DetailAndUser.style";
+import { capstoneAxios } from "../../axios-instance";
+import { ROLE } from "../../constants";
 
 const DetailItemAdmin = ({ isOpen, handleClose }: DetailAndEmployeeModal) => {
   const { id } = useParams();
+  const idNum = Number(id);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const role = localStorage.getItem("role")!;
 
-  // dummy
-  const isMaintenance = false;
+  const { isLoading, isError, error, data } = useQuery(
+    ["getItems", id],
+    async () => {
+      const { data } = await capstoneAxios({
+        method: "GET",
+        url: `/items/${idNum}`,
+      });
 
-  const maintenanceButton = isMaintenance ? (
-    <Button variant="contained" size="small" sx={buttonActions}>
-      Pemeliharaan Aset Selesai
-    </Button>
-  ) : (
-    <Button variant="contained" size="small" sx={buttonActions}>
-      Pemeliharaan Aset
-    </Button>
+      return data;
+    },
+    {
+      enabled: idNum && idNum !== 0 && role === ROLE.ADMIN ? true : false,
+      retry: 0,
+    }
   );
+
+  const {
+    isLoading: isLoadingMutation,
+    isError: isErrorMutation,
+    error: errorMutation,
+    isSuccess,
+    mutateAsync,
+  } = useMutation(
+    async (newStatus: string) => {
+      const { data } = await capstoneAxios({
+        method: "PUT",
+        url: `/items/${id}`,
+        data: { availableStatus: newStatus },
+      });
+    },
+    { onSuccess: () => queryClient.invalidateQueries("directoryAssetAdmin") }
+  );
+
+  if (isError) {
+    return <Error message={(error as AxiosError).response!.data!.message!} />;
+  }
+
+  if (isErrorMutation) {
+    return (
+      <Error message={(errorMutation as AxiosError).response!.data!.message!} />
+    );
+  }
+
+  if (isSuccess) {
+    return <Success message="Berhasil mengubah status item" />;
+  }
+
+  const handleMaintenance = async (status: string) => {
+    await mutateAsync(status);
+    navigate("/direktori-aset");
+  };
+
+  const getMaintenanceButton = (status: string) => {
+    const label =
+      status === "tersedia" ? "Pemeliharaan Aset" : "Pemeliharaan Aset Selesai";
+
+    const updateStatusData =
+      status === "tersedia" ? "pemeliharaan" : "tersedia";
+
+    return (
+      <LoadingButton
+        variant="contained"
+        size="small"
+        sx={buttonActions}
+        onClick={() => handleMaintenance(updateStatusData)}
+        loadingIndicator="Loading..."
+        loading={isLoadingMutation}
+      >
+        {label}
+      </LoadingButton>
+    );
+  };
 
   return (
     <Dialog
@@ -51,32 +125,42 @@ const DetailItemAdmin = ({ isOpen, handleClose }: DetailAndEmployeeModal) => {
           </Typography>
         </Box>
       </DialogTitle>
-      <DialogContent>
-        <img
-          src="https://source.unsplash.com/random"
-          alt="Aset"
-          style={detailImg}
-        />
-        <CustomFormRead label="Nama Aset" defaultValue="Lenovo" />
-        <CustomFormReadMulti
-          label="Deskripsi Aset"
-          defaultValue="Ini adalah laptop yang akan digunakan untuk coding"
-        />
-        <CustomFormRead label="Kategori Aset" defaultValue="Laptop" />
+      {isLoading && <Loading />}
+      {data && (
+        <DialogContent>
+          <img src={data.data.picture} alt="Aset" style={detailImg} />
+          <CustomFormRead label="Nama Aset" defaultValue={data.data.name} />
+          <CustomFormReadMulti
+            label="Deskripsi Aset"
+            defaultValue={data.data.description}
+          />
+          <CustomFormRead
+            label="Kategori Aset"
+            defaultValue={data.data.category}
+          />
+          <CustomFormRead
+            label="Status"
+            defaultValue={
+              data.data.availableStatus[0].toUpperCase() +
+              data.data.availableStatus.slice(1)
+            }
+          />
 
-        <Box sx={containerActionsDetail}>
-          {maintenanceButton}
+          <Box sx={containerActionsDetail}>
+            {["tersedia", "pemeliharaan"].includes(data.data.availableStatus) &&
+              getMaintenanceButton(data.data.availableStatus)}
 
-          <Button
-            variant="contained"
-            size="small"
-            sx={buttonActions}
-            onClick={() => handleClose(true)}
-          >
-            Kembali
-          </Button>
-        </Box>
-      </DialogContent>
+            <Button
+              variant="contained"
+              size="small"
+              sx={buttonActions}
+              onClick={() => handleClose(true)}
+            >
+              Kembali
+            </Button>
+          </Box>
+        </DialogContent>
+      )}
     </Dialog>
   );
 };
