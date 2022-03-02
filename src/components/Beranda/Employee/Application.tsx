4a2092711b18dcdc9d69da7efb86735c27a5eb4f
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-03-03 00:11:08 +0700

    feat: create applications by employee

--- src/components/Beranda/Employee/Application.tsx
+++ src/components/Beranda/Employee/Application.tsx
@@ -16,41 +16,77 @@ import { useNavigate } from "react-router-dom";
 import CustomFormInput from "../../CustomFormInput";
 import CustomFormSelect from "../../CustomFormSelect";
 import { backButton, cancellationButton } from "./DetailActivity.style";
+import { useMutation, useQuery } from "react-query";
+import { capstoneAxios } from "../../../axios-instance";
+import Loading from "../../Loading";
+import Error from "../../Alert/Error";
+import { AxiosError } from "axios";
 
 const Application = () => {
-  // dummy
-  const categories = ["kategori1", "kategori2", "kategori3"];
-  const assets = ["laptop lenovo", "laptop acer", "laptop asus"];
-
   const [isOpen, setIsOpen] = useState(true);
-  const [category, setCategory] = useState(categories[0]);
-  const [asset, setAsset] = useState(assets[0]);
+  const [asset, setAsset] = useState("");
 
   const navigate = useNavigate();
 
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const { isLoading, isError, error, data } = useQuery(
+    "assignAssetData",
+    async () => {
+      const { data } = await capstoneAxios({
+        method: "GET",
+        url: "/assets",
+        params: { status: "tersedia" },
+      });
+
+      return data;
+    },
+    { retry: 0 }
+  );
+
+  const { mutateAsync } = useMutation(async (newData: any) => {
+    await capstoneAxios({
+      method: "POST",
+      url: "/applications",
+      data: newData,
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")!}` },
+    });
+  });
+
   const handleClose = () => {
     setIsOpen(false);
     navigate("/beranda");
-  };
-
-  const handleChangeCategory = (e: any) => {
-    setCategory(e.target.value);
   };
 
   const handleChangeAsset = (e: any) => {
     setAsset(e.target.value);
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("tersubmit");
+
+    const formData = new FormData(e.currentTarget);
+    const asset = data.data.assets.filter(
+      (el: any) => el.name === formData.get("name")
+    );
+    const employeeId = localStorage.getItem("id")!;
+
+    await mutateAsync({
+      employeeid: parseInt(employeeId),
+      assetid: (asset[0] as any).id,
+      specification: formData.get("spec"),
+      description: formData.get("description"),
+      returndate: formData.get("date"),
+    });
 
     setIsOpen(false);
     navigate("/beranda");
   };
+
+  if (isError) {
+    return <Error message={(error as AxiosError).response!.data!.message!} />;
+  }
 
   return (
     <Dialog
@@ -67,44 +103,46 @@ const Application = () => {
         </Box>
       </DialogTitle>
 
-      <DialogContent>
-        <Box component="form" onSubmit={handleSubmit}>
-          <CustomFormSelect
-            label="Kategori"
-            desc="kategori"
-            selections={categories}
-            value={category}
-            handleChange={handleChangeCategory}
-          />
-          <CustomFormSelect
-            label="Nama Aset"
-            desc="nama-aset"
-            selections={assets}
-            value={asset}
-            handleChange={handleChangeAsset}
-          />
-          <CustomFormInput
-            label="Spesifikasi Kebutuhan"
-            type="text"
-            desc="spesifikasi-kebutuhan"
-            placeholder="Deskripsikan barang yang dibutuhkan"
-          />
-          <CustomFormInput
-            label="Keterangan"
-            type="text"
-            desc="keterangan"
-            placeholder="Deskripsikan tujuan penggunaan barang yang akan dipinjam"
-          />
-          <DialogActions>
-            <Button sx={cancellationButton} onClick={handleClose}>
-              Batal
-            </Button>
-            <Button type="submit" variant="contained" sx={backButton}>
-              Ajukan Peminjaman
-            </Button>
-          </DialogActions>
-        </Box>
-      </DialogContent>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <DialogContent>
+          <Box component="form" onSubmit={handleSubmit}>
+            <CustomFormSelect
+              label="Nama Aset"
+              desc="name"
+              selections={data.data.assets.map((el: any) => el.name)}
+              value={asset}
+              handleChange={handleChangeAsset}
+            />
+            <CustomFormInput
+              label="Spesifikasi Kebutuhan"
+              type="text"
+              desc="spec"
+              placeholder="Deskripsikan barang yang dibutuhkan"
+            />
+            <CustomFormInput
+              label="Deskripsi"
+              type="text"
+              desc="description"
+              placeholder="Deskripsikan tujuan penggunaan barang yang akan dipinjam"
+            />
+            <CustomFormInput
+              label="Tanggal Pengembalian"
+              type="date"
+              desc="date"
+            />
+            <DialogActions>
+              <Button sx={cancellationButton} onClick={handleClose}>
+                Batal
+              </Button>
+              <Button type="submit" variant="contained" sx={backButton}>
+                Ajukan Peminjaman
+              </Button>
+            </DialogActions>
+          </Box>
+        </DialogContent>
+      )}
     </Dialog>
   );
 };
