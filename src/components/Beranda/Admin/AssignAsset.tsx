4ccc7bcc70217fa79b08dd9ca9ae94d3f5f7fe2a
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-03-02 23:51:42 +0700

    feat: create assign asset by admin

--- src/components/Beranda/Admin/AssignAsset.tsx
+++ src/components/Beranda/Admin/AssignAsset.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import {
   Dialog,
@@ -11,21 +12,22 @@ import {
   DialogActions,
   Button,
 } from "@mui/material";
+import { AxiosError } from "axios";
+
+import CustomFormInput from "../../CustomFormInput";
+import CustomFormSelect from "../../CustomFormSelect";
+import Error from "../../Alert/Error";
 
 import { generalFont } from "../../../styles/font.style";
-import CustomFormInput from "../../CustomFormInput";
 import { leftButton, rightButton } from "./AddNewAsset.style";
-import CustomFormSelect from "../../CustomFormSelect";
+import { capstoneAxios } from "../../../axios-instance";
+import Loading from "../../Loading";
 
 const AssignAsset = () => {
-  // dummy
-  const categories = ["kategori1", "kategori2", "kategori3"];
-  const assets = ["lenovo", "asus", "acer"];
-  const employees = ["ratu", "jemi", "eldy", "najib"];
-
-  const [category, setCategory] = useState(categories[0]);
-  const [asset, setAsset] = useState(assets[0]);
-  const [employee, setEmployee] = useState(employees[0]);
+  const [asset, setAsset] = useState("");
+  const [employee, setEmployee] = useState("");
+  const [assets, setAssets] = useState([]);
+  const [employees, setEmployees] = useState([]);
   const [isOpen, setIsOpen] = useState(true);
 
   const navigate = useNavigate();
@@ -33,13 +35,38 @@ const AssignAsset = () => {
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
+  const { isLoading, isError, error } = useQuery(
+    "assignAssetData",
+    async () => {
+      const { data: employees } = await capstoneAxios({
+        method: "GET",
+        url: "/employees",
+      });
+
+      const { data: assets } = await capstoneAxios({
+        method: "GET",
+        url: "/assets",
+        params: { status: "tersedia" },
+      });
+
+      setEmployees(employees.data);
+      setAssets(assets.data.assets);
+    },
+    { retry: 0 }
+  );
+
+  const { mutateAsync } = useMutation(async (newData: any) => {
+    await capstoneAxios({
+      method: "POST",
+      url: "/applications",
+      data: newData,
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")!}` },
+    });
+  });
+
   const handleClose = () => {
     setIsOpen(false);
     navigate("/beranda");
-  };
-
-  const handleChangeCategory = (e: any) => {
-    setCategory(e.target.value);
   };
 
   const handleChangeAsset = (e: any) => {
@@ -50,16 +77,30 @@ const AssignAsset = () => {
     setEmployee(e.target.value);
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // log data
     const data = new FormData(e.currentTarget);
-    console.log(data.get("date"));
+    const asset = assets.filter((el: any) => el.name === data.get("nama-aset"));
+    const employee = employees.filter(
+      (el: any) => el.name === data.get("karyawan")
+    );
+
+    await mutateAsync({
+      employeeid: (employee[0] as any).id,
+      assetid: (asset[0] as any).id,
+      specification: data.get("spec"),
+      description: data.get("description"),
+      returndate: data.get("date"),
+    });
 
     setIsOpen(false);
     navigate("/beranda");
   };
+
+  if (isError) {
+    return <Error message={(error as AxiosError).response!.data!.message!} />;
+  }
 
   return (
     <Dialog
@@ -71,51 +112,52 @@ const AssignAsset = () => {
       <DialogTitle>
         <Box>
           <Typography variant="h5" sx={generalFont}>
-            Tambah Aset Baru {employee} {asset} {category}
+            Assign Asset
           </Typography>
         </Box>
       </DialogTitle>
 
-      <DialogContent>
-        <Box component="form" onSubmit={handleSubmit}>
-          <CustomFormSelect
-            label="Kategori Aset"
-            desc="kategori-aset"
-            selections={categories}
-            value={category}
-            handleChange={handleChangeCategory}
-          />
-          <CustomFormSelect
-            label="Nama Aset"
-            desc="nama-aset"
-            selections={assets}
-            value={asset}
-            handleChange={handleChangeAsset}
-          />
-          <CustomFormSelect
-            label="Karyawan"
-            desc="karyawan"
-            selections={employees}
-            value={employee}
-            handleChange={handleChangeEmployee}
-          />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <DialogContent>
+          <Box component="form" onSubmit={handleSubmit}>
+            <CustomFormSelect
+              label="Nama Aset"
+              desc="nama-aset"
+              selections={assets.map((asset: any) => asset.name)}
+              value={asset}
+              handleChange={handleChangeAsset}
+            />
+            <CustomFormSelect
+              label="Karyawan"
+              desc="karyawan"
+              selections={employees.map((employee: any) => employee.name)}
+              value={employee}
+              handleChange={handleChangeEmployee}
+            />
 
-          <CustomFormInput
-            label="Tanggal Pengembalian"
-            type="date"
-            desc="date"
-          />
+            <CustomFormInput
+              label="Tanggal Pengembalian"
+              type="date"
+              desc="date"
+            />
 
-          <DialogActions>
-            <Button sx={leftButton} onClick={handleClose}>
-              Kembali
-            </Button>
-            <Button type="submit" variant="contained" sx={rightButton}>
-              Tambahkan Aset
-            </Button>
-          </DialogActions>
-        </Box>
-      </DialogContent>
+            <CustomFormInput label="Deskripsi" type="text" desc="description" />
+
+            <CustomFormInput label="Spesifikasi" type="text" desc="spec" />
+
+            <DialogActions>
+              <Button sx={leftButton} onClick={handleClose}>
+                Kembali
+              </Button>
+              <Button type="submit" variant="contained" sx={rightButton}>
+                Assign
+              </Button>
+            </DialogActions>
+          </Box>
+        </DialogContent>
+      )}
     </Dialog>
   );
 };
