4748275d534e5edc7539b23f10c50e2d53c4399e
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-03-03 01:29:33 +0700

    feat: statistics

--- src/components/Beranda/Statistics.tsx
+++ src/components/Beranda/Statistics.tsx
@@ -1,4 +1,7 @@
 import { Box } from "@mui/material";
+import { useQuery } from "react-query";
+import { capstoneAxios } from "../../axios-instance";
+import Loading from "../Loading";
 import {
   botInnContainer,
   outerContainer,
@@ -7,17 +10,33 @@ import {
 import StatisticsBox from "./StatisticsBox";
 
 const Statistics = () => {
+  const { data, isLoading } = useQuery("getStatistics", async () => {
+    const { data } = await capstoneAxios({
+      method: "GET",
+      url: "/statistics",
+    });
+
+    return data;
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Box sx={outerContainer}>
         <Box sx={topInnContainer}>
-          <StatisticsBox total={102} label="Total Aset" />
-          <StatisticsBox total={93} label="Pemeliharaan" />
+          <StatisticsBox total={data.data.totalAsset} label="Total Aset" />
+          <StatisticsBox
+            total={data.data.totalMaintenance}
+            label="Pemeliharaan"
+          />
         </Box>
 
         <Box sx={botInnContainer}>
-          <StatisticsBox total={310} label="Digunakan" />
-          <StatisticsBox total={22} label="Tersedia" />
+          <StatisticsBox total={data.data.totalUsed} label="Digunakan" />
+          <StatisticsBox total={data.data.totalAvailable} label="Tersedia" />
         </Box>
       </Box>
     </>
