3e2a67e79614c4375f61e12976facc682445c6d4
Jemi Yantika Damanik <jemidamanik2300@gmail.com>
2022-03-03 08:35:54 +0700

    feat : functionality penngadaan aset

--- src/pages/PengadaanAset.tsx
+++ src/pages/PengadaanAset.tsx
@@ -1,38 +1,140 @@
-import React from "react";
 import {
   button,
   buttonOutlined,
-  buttonStatus,
-} from "../components/PengadaanAset/UpperButton.style";
+  buttonSelect,
+  buttonStatusPengadaanAset,
+} from "../components/PermohonanPengadaan/UpperButton.style";
+import React from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { AxiosError } from "axios";
+import { useState, MouseEvent } from "react";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { makeStyles } from "@mui/styles";
 import { bgwhite, primary, quaternary, tertiary } from "../styles/color.styles";
-import { useState, MouseEvent } from "react";
-import { useNavigate } from "react-router-dom";
-import { useStyles } from "../components/PengadaanAset/table.style";
-import { Box, IconButton, Menu, MenuItem, Typography } from "@mui/material";
+import { useStyles } from "../components/PermohonanPengadaan/table.style";
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+  Button,
+  ButtonGroup,
+} from "@mui/material";
 import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
-
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuList from "@mui/material/MenuList";
+import { capstoneAxios } from "../axios-instance";
+import Loading from "../components/Loading";
+import Error from "../components/Alert/Error";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { idText } from "typescript";
+import { useMutation } from "react-query";
 
 const PengadaanAset = () => {
-  const [filter, setFilter] = useState("");
+  const role = localStorage.getItem("role");
+  const [items, setItems] = useState<any[]>([]);
+  const [data, setData] = useState<any[]>([]);
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const [filter, setFilter] = useState("Semua Pengguna");
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
   const [clickedMenuId, setClickedMenuId] = useState(0);
   const classes = useStyles();
   const [isOpenDetail, setIsOpenDetail] = useState(false);
   const [isOpenUser, setIsOpenUser] = useState(false);
-  const [status, setStatus] = useState("");
-  const navigate = useNavigate();
+  const [status, setStatus] = useState("all");
+  const [statusItem, setStatusItem] = useState("");
+  const [newStatusItem, setNewStatusItem] = useState("");
 
-  const handleClickAction = (e: MouseEvent<HTMLElement>, id: number) => {
-    setStatus("toadmin");
+  const navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const [mbDdown, setMbDdown] = useState("5%");
+
+  const queryClient = useQueryClient();
+
+  let { isLoading, error, isError, refetch } = useQuery(
+    ["ProcurementsByStatus", status],
+    async () => {
+      const { data } = await capstoneAxios({
+        method: "GET",
+        url: "/procurements",
+        params: {
+          status: status,
+        },
+      });
+      if (data.data) {
+        setData(data.data);
+      } else {
+        setData([]);
+      }
+      return data;
+    },
+    { enabled: status !== "all" ? true : false }
+  );
+
+  const { mutateAsync } = useMutation(
+    async (newStatus: any) => {
+      await capstoneAxios({
+        method: "PUT",
+        data: { status: newStatus },
+        url: `/procurements/` + clickedMenuId,
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")!}` },
+      });
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("ProcurementsByStatus");
+        queryClient.invalidateQueries("allProcurements");
+      },
+    }
+  );
+
+  ({ isLoading, error, isError } = useQuery(["allProcurements"], async () => {
+    const { data } = await capstoneAxios({
+      method: "GET",
+      url: "/procurements",
+    });
+
+    if (data.data) {
+      setItems(data.data);
+    } else {
+      setItems([]);
+    }
+    console.log(data.data);
+
+    return data;
+  }));
+
+  if (isLoading) {
+    return (
+      <Box sx={{ marginTop: 25 }}>
+        <Loading />
+      </Box>
+    );
+  }
+
+  if (isError) {
+    return <Error message={(error as AxiosError).response!.data!.message!} />;
+  }
+
+  const handleClickAction = (
+    e: MouseEvent<HTMLElement>,
+    id: number,
+    params: any
+  ) => {
     setAnchorElUser(e.currentTarget);
     setClickedMenuId(id);
+    console.log(params.row.status);
   };
 
   const handleCloseDetail = () => {
@@ -53,17 +155,25 @@ const PengadaanAset = () => {
     navigate("/pengadaan-aset");
   };
 
-  const handleAcceptRequest = () => {
-    console.log(clickedMenuId);
+  const handleAcceptRequest = async () => {
+    await mutateAsync("tomanager");
+    setAnchorElUser(null);
+    navigate("/pengadaan-aset");
   };
 
-  const handleRejectRequest = () => {
+  const handleRejectRequest = async () => {
+    await mutateAsync("decline");
+    setAnchorElUser(null);
+  };
+
+  const handleModal = () => {
+    setAnchorElUser(null);
     navigate(`/pengadaan-aset/` + clickedMenuId);
   };
 
   const columns: GridColDef[] = [
     {
-      field: "nomor",
+      field: "id",
       headerClassName: "super-app-theme--header",
       cellClassName: "super-app-theme--cell",
       headerName: "No",
@@ -71,32 +181,33 @@ const PengadaanAset = () => {
       width: 50,
     },
     {
-      field: "tanggalPeminjaman",
+      field: "requestDate",
       headerClassName: "super-app-theme--header",
       cellClassName: "super-app-theme--cell",
       headerName: "Tanggal",
       width: 190,
     },
     {
-      field: "pemohonPeminjaman",
+      field: "employeeName",
       headerClassName: "super-app-theme--header",
       cellClassName: "super-app-theme--cell",
       headerName: "Pemohon",
       type: "date",
       width: 200,
     },
+
     {
-      field: "kategori",
-      headerClassName: "super-app-theme--header",
-      cellClassName: "super-app-theme--cell",
-      headerName: "Kategori Aset",
-      width: 180,
-    },
-    {
-      field: "namaAset",
+      field: "assetName",
       headerClassName: "super-app-theme--header",
       cellClassName: "super-app-theme--cell",
       headerName: "Barang",
+      width: 250,
+    },
+    {
+      field: "spesification",
+      headerClassName: "super-app-theme--header",
+      cellClassName: "super-app-theme--cell",
+      headerName: "Spesifikasi",
       width: 250,
     },
 
@@ -111,10 +222,15 @@ const PengadaanAset = () => {
       renderCell: (params: any) => {
         return (
           <>
-            <IconButton onClick={e => handleClickAction(e, params.id)}>
+            <IconButton
+              onClick={e => {
+                setStatusItem(params.row.status);
+                handleClickAction(e, params.id, params);
+              }}
+            >
               <MoreHorizRoundedIcon fontSize="medium" />
             </IconButton>
-            {status === "toadmin" && (
+            {statusItem === "toadmin" ? (
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorElUser}
@@ -130,24 +246,19 @@ const PengadaanAset = () => {
                 open={Boolean(anchorElUser)}
                 onClose={() => setAnchorElUser(null)}
               >
-                <Link
-                  to={`/pengadaan-aset/` + 2}
-                  style={{ textDecoration: "none", width: "100%" }}
-                >
-                  <MenuItem onClick={handleAcceptRequest}>
-                    <Typography
-                      variant="subtitle2"
-                      sx={{
-                        ...primary,
-                        textAlign: "center",
-                        fontFamily: "Poppins",
-                        fontWeight: "medium",
-                      }}
-                    >
-                      Terima Permohonan
-                    </Typography>
-                  </MenuItem>
-                </Link>
+                <MenuItem onClick={handleAcceptRequest}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      ...primary,
+                      textAlign: "center",
+                      fontFamily: "Poppins",
+                      fontWeight: "medium",
+                    }}
+                  >
+                    Terima Permohonan
+                  </Typography>
+                </MenuItem>
 
                 <MenuItem onClick={handleRejectRequest}>
                   <Typography
@@ -162,6 +273,50 @@ const PengadaanAset = () => {
                     Tolak Permohonan
                   </Typography>
                 </MenuItem>
+
+                <MenuItem onClick={handleModal}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      ...primary,
+                      textAlign: "center",
+                      fontFamily: "Poppins",
+                      fontWeight: "medium",
+                    }}
+                  >
+                    Lihat Detail
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            ) : (
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={() => setAnchorElUser(null)}
+              >
+                <MenuItem onClick={handleModal}>
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      ...primary,
+                      textAlign: "center",
+                      fontFamily: "Poppins",
+                      fontWeight: "medium",
+                    }}
+                  >
+                    Lihat Detail
+                  </Typography>
+                </MenuItem>
               </Menu>
             )}
           </>
@@ -170,95 +325,43 @@ const PengadaanAset = () => {
     },
   ];
 
-  const rows = [
-    {
-      id: 1,
-      nomor: 1,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Jemi",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 2,
-      nomor: 2,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Ratu",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 3,
-      nomor: 3,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Eldy",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 4,
-      nomor: 4,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Najib",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 5,
-      nomor: 5,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Jemi",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 6,
-      nomor: 6,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Ratu",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 7,
-      nomor: 7,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Eldy",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-    {
-      id: 8,
-      nomor: 8,
-      tanggalPeminjaman: new Date(),
-      pemohonPeminjaman: "Najib",
-      kategori: "Laptop",
-      namaAset: "Lenovo Think Pad Yoga",
-      status: "Digunakan",
-    },
-  ];
-  const fetchNav = async (data: any) => {
-    console.log(data);
-  };
-  const fetchData = async () => {
-    console.log("all");
+  const handleFilter = (item: string) => {
+    console.log(status);
   };
 
-  const handleFilter = (item: string) => {
-    fetchNav(item);
-    setFilter(item);
+  const handleClick = () => {};
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+    setMbDdown("30%");
   };
+
+  const handleMenuItemClick = (item: any) => {
+    setSelectedIndex(item.id - 1);
+    setOpen(false);
+    // console.log(item.id);
+    setMbDdown("5%");
+  };
+
+  const handleAllMenuClick = () => {
+    setSelectedIndex(100);
+    setOpen(false);
+  };
+
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <Layout>
       <Header
-        title="Pengadaan Aset"
+        title="Permohonan Pengadaan"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
           dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
@@ -272,50 +375,135 @@ const PengadaanAset = () => {
           display: { xs: "none", sm: "none", md: "block" },
         }}
       >
-        <Button
+        <ButtonGroup
           variant="contained"
-          sx={filter === "" ? button : buttonOutlined}
-          size="small"
-          onClick={data => {
-            setFilter("");
-            fetchData();
+          aria-label="contained primary button group"
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            backgroundColor: "white",
+            boxShadow: 0,
           }}
         >
-          Semua Pengguna
-        </Button>
-        {buttonStatus.map(item => (
-          <Button
-            key={item.id}
-            variant="contained"
-            size="small"
-            sx={filter === item.name ? button : buttonOutlined}
-            onClick={() => {
-              setFilter(item.name);
-              handleFilter(item.status);
-            }}
-          >
-            {item.name}
+          {buttonStatusPengadaanAset.map(item => (
+            <Button
+              key={item.id}
+              variant="contained"
+              size="small"
+              sx={filter === item.name ? button : buttonOutlined}
+              onClick={() => {
+                setFilter(item.name);
+                setStatus(item.status);
+                handleFilter(item.status);
+              }}
+            >
+              {item.name}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </Box>
+      <Box
+        sx={{
+          marginBottom: `${mbDdown}`,
+          marginTop: "5%",
+          marginLeft: "2%",
+          display: { sx: "block", sm: "block", md: "none" },
+        }}
+      >
+        <ButtonGroup
+          variant="contained"
+          ref={anchorRef}
+          aria-label="split button"
+        >
+          <Button sx={buttonSelect} onClick={handleClick}>
+            {selectedIndex <= 10
+              ? buttonStatusPengadaanAset[selectedIndex].name
+              : "Semua Pengguna"}
           </Button>
-        ))}
+          <Button
+            sx={buttonSelect}
+            size="small"
+            aria-controls={open ? "split-button-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-label="select merge strategy"
+            aria-haspopup="menu"
+            onClick={handleToggle}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
+
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList id="split-button-menu">
+                    <MenuItem
+                      onClick={() => {
+                        handleAllMenuClick();
+                      }}
+                    >
+                      Semua Pengguna
+                    </MenuItem>
+                    {buttonStatusPengadaanAset.map(item => (
+                      <MenuItem
+                        key={item.id}
+                        selected={item.id === selectedIndex}
+                        onClick={() => {
+                          setStatus(item.status);
+                          handleMenuItemClick(item);
+                        }}
+                      >
+                        {item.name}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
       </Box>
 
       <Outlet />
-      <Box sx={{ textAlign: "center" }}>
+      <Box
+        sx={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
         <Box
           sx={{
-            height: 600,
-            width: "100%",
+            height: 10 * 60 + 70,
+            width: { xs: "100%", sm: "100%", md: "85%" },
             marginTop: 3,
-            marginBottom: 4,
-            marginLeft: { sx: 0, sm: 0, md: "10%" },
-            marginRight: { sx: 0, sm: 0, md: "10%" },
+            marginBottom: 1,
+            // marginLeft: { sx: 0, sm: 0, md: "10%" },
+            // marginRight: { sx: 0, sm: 0, md: "10%" },
             textAlign: "center",
             alignItems: "center",
           }}
           className={classes.root}
         >
           <DataGrid
-            rows={rows}
+            rows={status === "all" ? items : data}
             columns={columns}
             pageSize={10}
             rowsPerPageOptions={[10]}
