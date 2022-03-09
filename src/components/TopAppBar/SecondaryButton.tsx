8e789fe5ad75bf01c0294379acc45db97d320bdd
Jemi Yantika Damanik <jemidamanik2300@gmail.com>
2022-03-09 21:37:52 +0700

    fix : add getColor props

--- src/components/TopAppBar/SecondaryButton.tsx
+++ src/components/TopAppBar/SecondaryButton.tsx
@@ -4,6 +4,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { ButtonLoading } from "../../types/button";
 import { secondaryButton } from "./SecondaryButton.style";
 
+import { theLightGreen, theGreen } from "../../styles/color.styles";
+
 const SecondaryButton = ({ label }: Partial<ButtonLoading>) => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -16,11 +18,19 @@ const SecondaryButton = ({ label }: Partial<ButtonLoading>) => {
     const path = getPath(label);
     navigate(path);
   };
+  const getColor = (label: string): string => {
+    const path = getPath(label);
+    if (location.pathname === path) {
+      return theGreen.color;
+    }
+
+    return theLightGreen.color;
+  };
 
   const getTextDecoration = (label: string): string => {
     const path = getPath(label);
     if (location.pathname === path) {
-      return "underline";
+      return "none";
     }
 
     return "none";
@@ -31,6 +41,7 @@ const SecondaryButton = ({ label }: Partial<ButtonLoading>) => {
       size="large"
       sx={{
         ...secondaryButton,
+        color: getColor(label as string),
         textDecoration: getTextDecoration(label as string),
         "&:hover": {
           textDecoration: getTextDecoration(label as string),
